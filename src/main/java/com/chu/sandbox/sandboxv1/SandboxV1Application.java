25d061ed75108603327205dc5f54cd4ac5b9c3ea
edmond-chu <edmond.chu1999@gmail.com>
2024-02-19 16:02:12 -0500

    initial commit

---
+++ src/main/java/com/chu/sandbox/sandboxv1/SandboxV1Application.java
@@ -0,0 +1,13 @@
+package com.chu.sandbox.sandboxv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandboxV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/chu/sandbox/sandboxv1/SandboxV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chu.sandbox.sandboxv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
